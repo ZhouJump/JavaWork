e8bd32d2e9826043a5cc630b7e0076ebfd98e452
zhoujump <zhouchujiede@126.com>
2022-10-10 19:37:57 +0800

    233

--- src/work3.java
+++ src/work3.java
@@ -1,2 +1,26 @@
 public class work3 {
+    public static void main(String args[])
+    {
+        int swap = 0;
+        int []num={10,78,12,15,46,47,2,10};
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                if (num[j] <num [j+1])
+                {
+                    swap = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = swap;
+                }
+
+            }
+        }
+        for (int l = 0; l < 7; l++)
+        {
+           System.out.println(num[l]+" ");
+        }
+
+
+    }
 }
