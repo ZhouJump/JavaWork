c1435e9d00d05966ebd0e9f56d239020c4f97079
zhoujump <zhouchujiede@126.com>
2022-10-10 19:50:47 +0800

    233

---
+++ src/work4.java
@@ -0,0 +1,19 @@
+public class work4 {
+    public static void main(String[] args) {
+        {
+            int i, j, k;
+            for (i = 1; i < 99999; i++) {
+                int sum = 0;
+                for (j = 1; j < i; j++) {
+                    if (i % j == 0) {
+                        sum += j;
+                    }
+                }
+                if (sum == i) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
+
